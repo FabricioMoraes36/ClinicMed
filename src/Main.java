2df227c9058bce958931f90815ee620fba5c50cc
Fabricio Moraes <Fabricio36365@outlook.com>
2025-06-19 23:59:40 -0300

    feat: Cria a opcao de remover o paciente por cpf

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,14 @@ public class Main {
         int pacientesCadastrados = 0;
         int opcao = 0;
 
-        while (opcao != 4) {
+        while (opcao != 5) {
             System.out.println("---Bem vindo ao ClinicsMed---");
             System.out.println("--Escolha uma opção--");
             System.out.println("-1 Cadastrar um usuario");
             System.out.println("-2 Listar os usuarios");
             System.out.println("-3 Buscar por cpf");
-            System.out.println("-4 sair");
+            System.out.println("-4 Remover paciente pelo cpf");
+            System.out.println("-5 sair");
             opcao = scanner.nextInt();
             scanner.nextLine();
 
@@ -23,20 +24,21 @@ public class Main {
                 case 1:Main.cadastrarPaciente(scanner, listaPacientes);
                 pacientesCadastrados++;
                     break;
-                case 2:
-                    System.out.println("A lista de pacientes é: " + listaPacientes);
 
+                case 2:Main.listar(listaPacientes);
                     break;
+
                 case 3:Main.buscarPacientePorCpf(scanner,listaPacientes);
                     break;
 
-                case 4:
+                case 4:Main.remover(scanner,listaPacientes);
+                    break;
+                case 5:
                     System.out.println("Finaliando...");
                     break;
             }
         }
     }
-
     public static void listar(List<Paciente>listaPacientes){
         if (listaPacientes.isEmpty()){
             System.out.println("A lista está vazia,tente novamente mais tarde...");
@@ -48,12 +50,26 @@ public class Main {
 
     }
     public static void cadastrarPaciente(Scanner scanner,List<Paciente>listaPacientes){
-        System.out.println("Digite o nome do paciente,idade e cpf: ");
+        System.out.println("Digite o nome do paciente:");
         String nomePaciente = scanner.nextLine();
+        System.out.println("A idade do paciente: ");
         int idadPaciente = scanner.nextInt();
+        System.out.println("O cpf do paciente: ");
+        boolean cpfBuscando = false;
         Long cpfPaciente = scanner.nextLong();
-        listaPacientes.add(new Paciente(nomePaciente,idadPaciente,cpfPaciente));
 
+        for (Paciente p : listaPacientes){
+            if (p.getCpf().equals(cpfPaciente)){
+                cpfBuscando = true;
+                break;
+            }
+
+        }
+        if(!cpfBuscando){
+            listaPacientes.add(new Paciente(nomePaciente,idadPaciente,cpfPaciente));
+            System.out.println("Paciente cadastrado com sucesso!");
+        }else{
+            System.out.println("Esse cpf ja esta cadastrado \n");        }
     }
     public static void buscarPacientePorCpf(Scanner scanner, List<Paciente> listaPacientes){
         System.out.println("Digite o cpf da pessoa que esta buscando: ");
@@ -69,6 +85,23 @@ public class Main {
         if(!encontrou){
             System.out.println("Não possuimos esse cpf nos registros...");
         }
+        }
+        public static void remover(Scanner sc,List<Paciente>listaPacientes){
+            System.out.println("Digite o cpf: ");
+            Long cpfBuscado = sc.nextLong();
+            boolean encontrou = false;
+            for (int i = 0; i < listaPacientes.size() ; i++) {
+                if (listaPacientes.get(i).getCpf().equals(cpfBuscado)){
+                    listaPacientes.remove(i);
+                    encontrou = true;
+                    System.out.println("Paciente removido com sucesso!!");
+                    break;
+                }
+            }
+            if(!encontrou){
+                System.out.println("Este cpf não está em nossos registros");
+            }
+
         }
 
     }
