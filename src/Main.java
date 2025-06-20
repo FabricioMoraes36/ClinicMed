f0257a2c0892b418611613057eccc72e5d336d4b
Fabricio Moraes <Fabricio36365@outlook.com>
2025-06-19 23:19:29 -0300

    feat: Adicionada a busca por cpf

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,13 @@ public class Main {
         int pacientesCadastrados = 0;
         int opcao = 0;
 
-        while (opcao != 3) {
+        while (opcao != 4) {
             System.out.println("---Bem vindo ao ClinicsMed---");
             System.out.println("--Escolha uma opção--");
             System.out.println("-1 Cadastrar um usuario");
             System.out.println("-2 Listar os usuarios");
-            System.out.println("-3 Sair");
+            System.out.println("-3 Buscar por cpf");
+            System.out.println("-4 sair");
             opcao = scanner.nextInt();
             scanner.nextLine();
 
@@ -26,11 +27,16 @@ public class Main {
                     System.out.println("A lista de pacientes é: " + listaPacientes);
 
                     break;
-                case 3:
+                case 3:Main.buscarPacientePorCpf(scanner,listaPacientes);
+                    break;
+
+                case 4:
                     System.out.println("Finaliando...");
+                    break;
             }
         }
     }
+
     public static void listar(List<Paciente>listaPacientes){
         if (listaPacientes.isEmpty()){
             System.out.println("A lista está vazia,tente novamente mais tarde...");
@@ -49,7 +55,25 @@ public class Main {
         listaPacientes.add(new Paciente(nomePaciente,idadPaciente,cpfPaciente));
 
     }
+    public static void buscarPacientePorCpf(Scanner scanner, List<Paciente> listaPacientes){
+        System.out.println("Digite o cpf da pessoa que esta buscando: ");
+        Long cpfBuscado = scanner.nextLong();
+        boolean encontrou = false;
+        for (Paciente p : listaPacientes){
+            if(p.getCpf().equals(cpfBuscado)){
+                System.out.println("O cpf é de : " + p + "\n" + ".");
+                encontrou = true;
+                break;
+            }
+            }
+        if(!encontrou){
+            System.out.println("Não possuimos esse cpf nos registros...");
+        }
+        }
+
     }
+
+
 
 
 
